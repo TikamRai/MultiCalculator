acb48ca184da9ce0eb4fb99de969449996acc36f
Tikam Rai <a00280792@mycambrian.ca>
2023-10-10 19:40:47 -0400

    Two Buttons Added

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.multicalculator.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -61,11 +63,17 @@ fun CalcNumericButton() {
 }
 
 @Composable
-fun CalcOperationButton() {
-
+fun CalcOperationButton(operation: String, display: MutableState<String>) {
+    ElevatedButton(modifier = Modifier.padding(4.dp),
+        onClick = { /*TODO*/ }) {
+        Text(text = operation)
+    }
 }
 
 @Composable
 fun CalcEqualsButton(display: MutableState<String>) {
-
+    ElevatedButton(modifier = Modifier.padding(4.dp),
+        onClick = {0}) {
+        Text(text = "=")
+    }
 }
